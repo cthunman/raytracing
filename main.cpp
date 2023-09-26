63174b23ee3a824e459050d6ac4e231a8c9d0b3a
Carl Thunman <thunman@gmail.com>
2023-09-25 22:27:39 -0400

    Finally finished the book.

--- main.cpp
+++ main.cpp
@@ -6,46 +6,94 @@
 #include "material.h"
 #include <random>
 
-vec3 color(const ray& r, hitable *world, int depth) {
+vec3 color(const ray &r, hitable *world, int depth)
+{
     hit_record rec;
-    if (world->hit(r, 0.001, MAXFLOAT, rec)) {
+    if (world->hit(r, 0.001, MAXFLOAT, rec))
+    {
         ray scattered;
         vec3 attenuation;
-        if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
-            return attenuation*color(scattered, world, depth + 1);
-        } else {
+        if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+        {
+            return attenuation * color(scattered, world, depth + 1);
+        }
+        else
+        {
             return vec3(0, 0, 0);
         }
-    } else {
+    }
+    else
+    {
         vec3 unit_direction = unit_vector(r.direction());
-        float t = 0.5*(unit_direction.y() + 1.0);
-        return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+        float t = 0.5 * (unit_direction.y() + 1.0);
+        return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
     }
 }
 
-int main() {
+hitable *random_scene()
+{
+    int n = 500;
+    hitable **list = new hitable *[n + 1];
+    list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+    int i = 1;
+    for (int a = -11; a < 11; a++)
+    {
+        for (int b = -11; b < 11; b++)
+        {
+            float choose_mat = drand48();
+            vec3 center = vec3(a + 0.9 * drand48(), 0.2, b + 0.9 * drand48());
+            if ((center - vec3(4, 0.2, 0)).length() > 0.9)
+            {
+                if (choose_mat < 0.8) // diffuse
+                {
+                    list[i++] = new sphere(center, 0.2, new lambertian(vec3(drand48() * drand48(), drand48() * drand48(), drand48() * drand48())));
+                }
+                else if (choose_mat < 0.95) // metal
+                {
+                    list[i++] = new sphere(center, 0.2, new metal(vec3(0.5 * (1 + drand48()), 0.5 * (1 + drand48()), 0.5 * (1 + drand48())), 0.5 * drand48()));
+                }
+                else // glass
+                {
+                    list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+                }
+            }
+        }
+    }
+    list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+    list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+    list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+
+    return new hitable_list(list, i);
+}
+
+int main()
+{
     std::mt19937 rng;
     rng.seed(std::random_device()());
     std::uniform_real_distribution<float> rand_float(0.0, 1.0);
 
-    int nx = 500;
-    int ny = 250;
-    int ns = 30;
-    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+    int nx = 1200;
+    int ny = 800;
+    int ns = 15;
+    std::cout << "P3\n"
+              << nx << " " << ny << "\n255\n";
 
-    hitable *list[4];
-    list[0] = new sphere(vec3(0,0,-1.5), 0.5, new metal(vec3(0.8, 0.3, 0.3)));
-    list[1] = new sphere(vec3(0,-100.5,-1.5), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
-    list[2] = new sphere(vec3(1,0,-1.5), 0.5, new metal(vec3(0.8, 0.6, 0.2)));
-    list[3] = new sphere(vec3(-1,0,-1.5), 0.5, new metal(vec3(0.8, 0.8, 0.8)));
-    hitable *world = new hitable_list(list, 4);
+    hitable *world = random_scene();
 
-    camera cam;
+    vec3 lookfrom(13,2,3);
+    vec3 lookat(0,0,0);
+    float dist_to_focus = 10.0;
+    float aperture = 0.1;
 
-    for (int j = ny-1; j >= 0; j--){
-        for (int i = 0; i < nx; i ++) {
+    camera cam(lookfrom, lookat, vec3(0.0, 1.0, 0.0), 20.0, float(nx) / float(ny), aperture, dist_to_focus);
+
+    for (int j = ny - 1; j >= 0; j--)
+    {
+        for (int i = 0; i < nx; i++)
+        {
             vec3 col(0, 0, 0);
-            for (int s=0; s < ns; s++) {
+            for (int s = 0; s < ns; s++)
+            {
                 float u = float(i + rand_float(rng)) / float(nx);
                 float v = float(j + rand_float(rng)) / float(ny);
                 ray r = cam.get_ray(u, v);
@@ -53,14 +101,13 @@ int main() {
                 col += color(r, world, 0);
             }
             col /= float(ns);
-            col = vec3( sqrt(col[0]), sqrt(col[1]), sqrt(col[2]) );
+            col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
 
-            int ir = int(255.99*col[0]);
-            int ig = int(255.99*col[1]);
-            int ib = int(255.99*col[2]);
+            int ir = int(255.99 * col[0]);
+            int ig = int(255.99 * col[1]);
+            int ib = int(255.99 * col[2]);
 
             std::cout << ir << " " << ig << " " << ib << "\n";
         }
     }
 }
-
